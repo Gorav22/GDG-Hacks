e61a8da175437825ba9be7d9d0327b93f9849180
Gorav <goravjindal86@gmail.com>
2025-02-19 18:03:41 +0530

    Library Work done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Home from './pages/Home';
 import Signup from './pages/Signup'
+import Library from './pages/Library';
+import Chat from './pages/Chat'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/chat" element={<Chat />} />
+          <Route path='/library' element={<Library/>}/>
           <Route path="/" element={<Home/>} />
         </Routes>
     </Router>
